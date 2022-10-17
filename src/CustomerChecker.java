db188ede2aeca8529995e70d38b4c1d1b22539ed
Abdullah Shah <abdullah3057@gmail.com>
2022-10-17 17:15:47 +0200

    Done some work on the assignment

--- src/CustomerChecker.java
+++ src/CustomerChecker.java
@@ -1,21 +1,34 @@
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class CustomerChecker {
-    DataList dataGetter = new DataList();
-    ArrayList<String> dataList = dataGetter.get("src/customers.txt");
-    for(int i = 0;i<dataList.size();i++){
 
-
-    }
-
-    public CustomerChecker() throws IOException {
-    }
-
-    public static void main(String[]args) throws IOException {
+    public void customerInfo(String filePath, String idOrName) throws IOException {
         DataList dataGetter = new DataList();
-        ArrayList<String> dataList = dataGetter.get("src/customers.txt");
-        System.out.println(dataList.size());
+        ArrayList<String> dataList = dataGetter.get(filePath);
+        LocalDate today = LocalDate.now();
+        LocalDate aYearAgo = today.minusYears(1);
+        String customerInfo = "";
+        for (int i = 0; i < dataList.size(); i++) {
+            if(!dataList.get(i).contains("-"))
+                if (dataList.get(i).split(", ")[0].equals(idOrName)
+                        ||dataList.get(i).split(", ")[1].equals(idOrName)){
+                    LocalDate memberDate = LocalDate.parse(dataList.get(i+1));
+                    if(memberDate.isBefore(aYearAgo)||memberDate.equals(aYearAgo)){
+                        customerInfo = "Personen är en före detta medlem.";
+                    }
+                    else{
+                        customerInfo = "Personen är en nuvarande medlem.";
+                    }
+                }
+                else
+                    customerInfo = "Personen har aldrig varit medlem.";
+        }
 
+        System.out.print(customerInfo);
+
+    }
+    public CustomerChecker() throws IOException {
     }
 }
