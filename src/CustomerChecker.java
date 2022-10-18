d92e83400e41dd0365be9933ef36a7c755aa2f58
Abdullah Shah <abdullah3057@gmail.com>
2022-10-18 14:08:13 +0200

    Done some work on the assignment

--- src/CustomerChecker.java
+++ src/CustomerChecker.java
@@ -1,36 +1,44 @@
+import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class CustomerChecker {
 
-    public String customerInfo(String filePath, String idOrName) throws IOException {
+    public String [] customerInfo(String filePath, String idOrName) throws IOException {
         DataList dataGetter = new DataList();
         ArrayList<String> dataList = dataGetter.get(filePath);
         LocalDate today = LocalDate.now();
         LocalDate aYearAgo = today.minusYears(1);
-        String customerInfo = "";
+        String visitorStatus = "";
+        String [] informationArray = new String[2];
+        String customerLog = "";
         for (int i = 0; i < dataList.size(); i++) {
             if(!dataList.get(i).contains("-"))
                 if (dataList.get(i).split(", ")[0].equals(idOrName)
                         ||dataList.get(i).split(", ")[1].equals(idOrName)){
                     LocalDate memberDate = LocalDate.parse(dataList.get(i+1));
                     if(memberDate.isBefore(aYearAgo)||memberDate.equals(aYearAgo)){
-                        customerInfo = "Personen är en före detta medlem.";
+                        visitorStatus = "Personen är en före detta medlem.";
                         break;
                     }
                     else{
-                        customerInfo = "Personen är en nuvarande medlem.";
+                        visitorStatus = "Personen är en nuvarande medlem.";
+                        customerLog+=dataList.get(i)+"\n"+today+"\n";
                         break;
                     }
                 }
                 else
-                    customerInfo = "Personen har aldrig varit medlem.";
+                    if(!idOrName.equals("x"))
+                    visitorStatus = "Personen har aldrig varit medlem.";
         }
 
-        return customerInfo;
+        informationArray[0] = visitorStatus;
+        informationArray[1] = customerLog;
+        return informationArray;
 
     }
+
     public CustomerChecker() throws IOException {
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,29 @@
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[]args) throws IOException {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Skriv in namn eller personnummer för att veta status för medlemskap.");
-        String input = scan.nextLine();
-        CustomerChecker customerChecker = new CustomerChecker();
-        System.out.println(customerChecker.customerInfo("src/customers.txt",input.trim()));
+        boolean runner = true;
+        String customerData = "";
+        File file = new File("src/customerVisits.txt");
+        PrintWriter outstream = new PrintWriter(new BufferedWriter(new FileWriter("customerVisits.txt")));
+        while(runner) {
+            System.out.println("Skriv in namn eller personnummer för att veta status för medlemskap. " +
+                    "Mata in x för att avsluta");
+            String input = scan.nextLine();
+            CustomerChecker customerChecker = new CustomerChecker();
+            System.out.println(customerChecker.customerInfo("src/customers.txt", input.trim())[0]);
+            if(customerChecker.customerInfo("src/customers.txt", input.trim())[0].equals(
+                    "Personen är en nuvarande medlem.")){
+                customerData+=customerChecker.customerInfo("src/customers.txt", input.trim())[1];
+            }
+            if(input.trim().equalsIgnoreCase("x")){
+                break;
+            }
+        }
+        outstream.print(customerData);
+        outstream.close();
+        }
     }
-}
